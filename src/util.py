0679f71b03aa9665d4690cc5e1feb2a917323286
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2023-03-29 17:37:25 +0200

    Add output writing function

--- src/util.py
+++ src/util.py
@@ -1,3 +1,4 @@
+import csv
 import re
 from copy import deepcopy as cp
 
@@ -63,6 +64,15 @@ def normalize_name(column_name):
     name = column_name.lower().strip(''.join(non_alphas))
     name = NON_ALPHA.sub('_', name)
     return f'_{name}'
+
+
+def write_output(df, out):
+    if out:
+        df.to_csv(out, index=False)
+    else:
+        for row in csv.writer(df.iterrows(index=False), quoting=csv.QUOTEALL):
+            out.write(row)
+    return True
 
 
 if __name__ == '__main__':
